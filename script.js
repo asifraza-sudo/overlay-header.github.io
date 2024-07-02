cb9079b860fd54c312d098fffa377c5dad5e0304
Harish Ahmed <letterspoint@gmail.com>
2024-07-02 19:20:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*function openOverlay() {
+    const overlay = document.getElementById('myOverlay');
+    overlay.classList.add('show');
+  }
+  
+  function closeOverlay() {
+    const overlay = document.getElementById('myOverlay');
+    overlay.classList.remove('show');
+  }*/
+  
